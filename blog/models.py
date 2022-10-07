18f138ec0a4a5f331b8fab145ec36d412aa3febb
kcw9609 <marie9609@naver.com>
2022-10-07 17:01:10 +0900

    blog, single_pages만들기

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    created_at = models.DateTimeField()
+    content = models.TextField()
+    #author 추후 작성
