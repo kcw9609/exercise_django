2c11d3fb168469619184b1211868d7c5cf149e81
kcw9609 <marie9609@naver.com>
2022-11-30 22:14:33 +0900

    some

--- shoppingMall/apps.py
+++ shoppingMall/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ShoppingmallConfig(AppConfig):
+class ShoppingMallConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'shoppingMall'
--- shoppingMall/views.py
+++ shoppingMall/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post, Category, Tag, Manufacturer
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.core.exceptions import PermissionDenied
+from django.utils.text import slugify
 
-# Create your views here.
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+class PostDetail(DetailView):
+    model = Post
